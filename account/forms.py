1184157d2a517013b22cf128eba05c3a96d27441
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-01-22 17:41:27 +0330

    cleaned_data
    clean-specific-form-field & override-clean

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,32 @@
 from django import forms
-
-
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 class UserRegisteForm(forms.Form):
     username = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     email= forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control','placeholder':'your-mail@gmial.com'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Password'}))
+
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this email is already in use')
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('this username is already in use')
+        return username
+    
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and  p1!=p2:
+            raise ValidationError('pass is not match !')
