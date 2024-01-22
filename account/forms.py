c3c44314c4396f7d0a78014a93f9e56c6c6191c4
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-01-22 12:32:23 +0330

    widgets
    use a different widgets for projects
    very widgets in django docs site

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class UserRegisteForm(forms.Form):
-    username = forms.CharField()
-    email= forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    email= forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control','placeholder':'your-mail@gmial.com'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Password'}))
