56c275ba4a666cd20b398529d522218b0bfdd09e
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-01-24 00:12:15 +0330

    delete post
    User information in the form of a link
    
    To delete user information 1detail.html
    2 urls app 3 views in app

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 # Create your views here.
 from .models import Post 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 class HomeView(View):
     def get(self, request):
         posts = Post.objects.all()
@@ -13,3 +15,15 @@ class PostDetailView(View):
     def get(self, request, post_id ,post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request , 'home/detail.html' , {'post':post})
+
+
+class PostDeleteView(LoginRequiredMixin,View):
+    def get(self,request,post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request,'Post has been deleted')
+
+        else:
+            messages.error(request,'you are not allowed to')
+            return redirect('home:home')
